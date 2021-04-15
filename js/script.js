4339aeb9fd6d76445f32db5dbe685dd5eaaab9a5
Stefano Patti <mastepa@hotmail.com>
2021-04-15 16:43:30 +0200

    ex done

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function init() {
+
+    for (let i = 0; i < 10; i++) {
+        getMailFromApi();
+    }
+}
+
+function getMailFromApi() {
+
+    $.ajax({
+        url: "https://flynn.boolean.careers/exercises/api/random/mail",
+        method: 'GET',
+
+        success: function (data) {                    
+                
+                const res = data.response;
+                mailGenerate(res);      
+        },
+
+        error: function () {
+            console.log('error');
+        }
+    });
+}
+
+function mailGenerate(type) {
+    const target = $('#target');
+  
+        target.append('<li style="color:red; list-style:none; font-weight: bold;">' + type + '</li> <br>');
+    
+}
+
+document.addEventListener('DOMContentLoaded', init);
