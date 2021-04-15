e22639b3a8cf1e230bc8006aaf8b925186b7180d
Stefano Patti <mastepa@hotmail.com>
2021-04-15 16:54:45 +0200

    levando una function e ciclando URL

--- js/script.js
+++ js/script.js
@@ -31,3 +31,41 @@ function mailGenerate(type) {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+// ----------------------------------------------
+
+// OPPURE DIRETTAMENTE:
+
+function init1() {
+
+
+    for (let i = 0; i < 10; i++) {
+        // importante è ciclare sempre url più volte
+        $.ajax({
+            url: "https://flynn.boolean.careers/exercises/api/random/mail",
+            method: 'GET',
+
+            success: function (data) {
+
+                const res = data.response;
+                mailGenerate(res);
+            },
+
+            error: function () {
+                console.log('error');
+            }
+        });
+
+    }
+}
+
+function mailGenerate(type) {
+    const target = $('#target');
+
+    target.append('<li style="color:red; list-style:none; font-weight: bold;">' + type + '</li> <br>');
+
+}
+
+document.addEventListener('DOMContentLoaded', init1);
